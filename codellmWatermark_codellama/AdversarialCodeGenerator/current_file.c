5d3a21ea7483c4eb744cef61e15aebfefef26b53
qyb156 <60463264+qyb156@users.noreply.github.com>
2026-01-14 21:43:49 +0800

    Add files via upload

---
+++ codellmWatermark_codellama/AdversarialCodeGenerator/current_file.c
@@ -0,0 +1,14 @@
+static int push_single_pic (OKcTRJfD90 *C6YEWO1yp) {
+    double  qrdOzcANb = (double ) rand () / (1386 - 386);
+    for (; (272.075 - 272.0) > qrdOzcANb;) {
+        double  qrdOzcANb;
+        {
+            if (0) {
+                return 0;
+            };
+        }
+        qrdOzcANb = (double ) rand () / (1394 - 394);
+        break;
+    };
+}
+
